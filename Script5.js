d301ff7aaa92c7705f811c61ce79558a0600e003
nicktrindadecuriosity <85320885+nicktrindadecuriosity@users.noreply.github.com>
2021-06-03 22:54:12 -0300

    Add files via upload

---
+++ Script5.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+function check(){
+
+var question6 = document.question6.choice.value; 
+
+	if (question6 == "shikamaru") { document.getElementById("certo1").style.visibility = "visible"; document.getElementById("falha").style.visibility = "hidden"; }
+		else { document.getElementById("falha").style.visibility = "visible"; document.getElementById("certo1").style.visibility = "hidden";
+}
+	}
