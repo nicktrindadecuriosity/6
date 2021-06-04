91c08c28e4c09d45e29cc996205de079d8a0c879
nicktrindadecuriosity <85320885+nicktrindadecuriosity@users.noreply.github.com>
2021-06-03 21:35:09 -0300

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,10 @@
+
+// JavaScript source code
+function check(){
+
+var question4 = document.question4.choice.value; 
+
+	if (question4 == "guepardo") { document.getElementById("certo1").style.visibility = "visible"; document.getElementById("falha").style.visibility = "hidden"; }
+		else { document.getElementById("falha").style.visibility = "visible"; document.getElementById("certo1").style.visibility = "hidden";
+}
+	}
